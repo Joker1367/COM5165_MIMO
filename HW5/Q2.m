254528e9d7da3fec7ec9e6c89fed3e380ac69d11
Joker1367 <henry3562202@gmail.com>
2024-11-19 09:04:39 +0800

    2024_1119

--- HW5/Q2.m
+++ HW5/Q2.m
@@ -1,36 +1,30 @@
 clear all
 clc
 
-% 1. Define parameters
-fc = 1e9;                  % Frequency (1 GHz)
-c = 3e8;                   % Speed of light (m/s)
-lambda = c / fc;           % Wavelength (m)
-elementNum = 4;            % Number of antenna elements
-Lr = 2;
-elementSpacing = lambda * Lr / elementNum; % Spacing between antenna elements
+% Initialize parameters
+fc = 3e9; % Operating frequency in Hz (e.g., 3 GHz)
+c = 3e8;  % Speed of light in m/s
+lambda = c / fc; % Wavelength in meters
+Lr = 8; % Total array length in wavelengths
+nr_values = [16]; % Different numbers of antennas
 
-% 2. Create a Uniform Linear Array (ULA)
-ula = phased.ULA('NumElements', elementNum, ...
-                 'ElementSpacing', elementSpacing);
+% Loop through different numbers of antennas
+for i = 1:length(nr_values)
+    nr = nr_values(i); % Number of antennas
+    d = lambda * Lr / nr; % Antenna element spacing (in wavelengths)
 
-% 3. Beamforming direction (e.g., beam points to [0, -90])
-beamDirection = [90; 0];  % Azimuth angle: 90 degrees, Elevation angle: 0 degrees
+    % Create the ULA (Uniform Linear Array)
+    array = phased.ULA('NumElements', nr, 'ElementSpacing', d);
 
-steervec = phased.SteeringVector('SensorArray', ula);
-w = steervec(fc, beamDirection);
+    % Compute steering vector
+    angles = -180:180; % Azimuth angle range
+    steeringVector = phased.SteeringVector('SensorArray', array, ...
+                                           'PropagationSpeed', c);
+    weights = steeringVector(fc, [0;90]); % Compute weights for 90° beam direction
 
-% 4. Calculate the radiation pattern and normalize
-angles = -180:180; % Azimuth angle range
-patternData = pattern(ula, fc, angles, 0, ...
-    'Type', 'power', ...    % Use power mode (linear scale)
-    'PropagationSpeed', c, ...
-    'Weight', w); 
-
-% Normalize to the maximum gain
-normalizedPattern = patternData / max(patternData); % Compute in linear scale
-
-% 5. Plot the beam pattern (polar coordinates, linear scale)
-figure;
-polarplot(deg2rad(angles), normalizedPattern, 'LineWidth', 1.5);
-title('Normalized ULA Beamforming Pattern (Linear Scale)');
-rlim([0 1]); % Set display range
+    % Plot the beam pattern
+    pattern(array, fc, angles, 0, ... 
+            'Type', 'directivity', ...
+            'PropagationSpeed', c, ...
+            'Weights', weights);
+end
